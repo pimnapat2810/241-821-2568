f230f15846e63b7b590546c78cf4bb487811e985
pimnapat2018 <pimnapat.ko@ku.th>
2026-02-17 11:47:39 +0700

    Commit

---
+++ Backend/index.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
+
+app.use(bodyParser.json());
+
+let users = []
+let counter = 1;
+
+//path = GET /users
+app.get('/users',  (req, res) => {
+    res.json(users);
+});
+
+//path = POST /user
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1
+    users.push(user);
+    res.json({ 
+        message: 'User added successfully', 
+        user: user });
+})
+
+//path = PUT /user/:id
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id
+    let updatedUser = req.body;
+    // หา users จาก id
+    let selectedIndex = users.findIndex(user => user.id == id)
+    //update users นั้น
+    if (updatedUser.name) {
+        users[selectedIndex].name = updatedUser.name || users[selectedIndex].name   
+    }
+    if (updatedUser.age) {
+        users[selectedIndex].age = updatedUser.age || users[selectedIndex].age
+    }
+
+    //ส่ง response กลับไปว่า update users ที่เลือกสำเร็จแล้ว
+
+    res.json({
+        message: 'User updated successfully',
+        data : {
+            user: updatedUser,
+            indexUpdated: selectedIndex
+        }
+    })  
+})
+
+//path = DELETE /users/:id
+app.delete('/users/:id', (req, res) => {
+    let id = req.params.id
+    let selectedIndex = users.findIndex(user => user.id == id)
+    if (selectedIndex !== -1) {
+        users.splice(selectedIndex, 1)
+        res.json({
+            message: 'User deleted successfully',
+            data: {
+                indexDeleted: selectedIndex
+            }
+        })
+    } else {
+        res.status(404).json({
+            message: 'User not found'
+        })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
