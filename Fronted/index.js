f1d48c7fe1634ac3c4bbe4a688efade90aeb2ebb
thanadonsaw-hue <thanadon.saw@ku.th>
2026-03-02 17:15:31 +0700

    Commit

--- Fronted/index.js
+++ Fronted/index.js
@@ -1,27 +1,41 @@
-function submitData () {
-    let firstNameDOM = document.querySelector('input[name=firstname]');
-    let lastNameDOM = document.querySelector('input[name=lastname]');
-    let ageDOM = document.querySelector('input[name=age]');
-    let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestDOMs = document.querySelectorAll('input[name=interest]:checked');
-    let descriptionDOM = document.querySelector('textarea[name=description]');
+const submitData = async () => {
+    let firstNameDOM = document.querySelector('input[name="firstname"]');
+    let lastNameDOM = document.querySelector('input[name="lastname"]');
+    let ageDOM = document.querySelector('input[name="age"]');
+    let genderDOM = document.querySelector('input[name="gender"]:checked');
+    let interestDOMs = document.querySelectorAll('input[name=interests]:checked');
+    let descriptionDOM = document.querySelector('textarea[name="description"]');
 
+    let messageDOM = document.getElementById('message');
+    
+    try {
     let interest = ''
-    for (let i = 0; i < interestDOMs.length; i++) {
-        interest += interestDOMs[i].value
-        if (i != interestDOMs.length - 1) {
-            interest += ','
+    for (let i=0; i < interestDOMs.length; i++){
+        interest += interestDOMs[i].value 
+        if (i !== interestDOMs.length -1){
+            interest += ', '
         }
     }
 
     let userData ={
-        firstName: firstNameDOM.value,
-        lastName: lastNameDOM.value,
+        firstname: firstNameDOM.value,
+        lastname: lastNameDOM.value,
         age: ageDOM.value,
         gender: genderDOM.value,
         description: descriptionDOM.value,
-        interest: interest
+        interests: interest
     }
-
-    console.log('submitData', userData);
-}
+    
+            const response = await axios.post("http://localhost:8000/users", userData)
+            console.log('response', response);
+            messageDOM.innerText = "บันทึกข้อมูลสำเร็จ!";
+            messageDOM.className = "message success";
+    } catch (error) {
+        if (error.response) {
+            console.log('Error response:', error.response.data);
+        }
+            messageDOM.innerText = "บันทึกข้อมูลไม่สำเร็จ!";
+            messageDOM.className = "message danger";
+    }
+    console.log('submit data: ', userData);
+    }
